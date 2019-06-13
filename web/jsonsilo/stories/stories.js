9c28c660bbf180223f58ab9881d86efdb7027afe
Furaha <furahadamien30@gmail.com>
2019-06-13 10:00:56 -0400

    final commit before branching

--- web/jsonsilo/stories/stories.js
+++ web/jsonsilo/stories/stories.js
@@ -16,18 +16,19 @@ let queryButton = document.querySelector('#queryButton');
 // Initialisation: GET the stories and display in DOM
 getStories(window.location.href, function(status, response) {
   jsonResponse.textContent = JSON.stringify(response, null, 2);
-  url.textContent = stories.url;
-  name.textContent = personName.value; //stories.name ;
- console.log(personName.value);
-  picture.textContent = stories.picture
+  //url.textContent = stories.url;
+  //name.textContent = personName.value; 
+ //console.log(response);
+  //picture.textContent = stories.picture
 });
 
 
 
 function getStories(url, callback) {
+  
   let httpRequest = new XMLHttpRequest();
-
   httpRequest.onreadystatechange = function() {
+    //console.log('SENT',httpRequest.responseText);
     if(httpRequest.readyState === XMLHttpRequest.DONE) {
       return callback(httpRequest.status, JSON.parse(httpRequest.responseText));
     }
@@ -36,22 +37,3 @@ function getStories(url, callback) {
   httpRequest.setRequestHeader('Accept', 'application/json');
   httpRequest.send();
 }
-
-function addStory() {
-  let url = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/stories/id';
-  let httpRequest = new XMLHttpRequest();
-  console.log('called');
-
-  httpRequest.onreadystatechange = function(){
-    if(httpRequest.readyState === XMLHttpRequest.DONE) {
-      if(httpRequest.status == 200){
-        let response = JSON.parse(httpRequest.responseText);
-      }
-    }
-  };
-  httpRequest.open('POST', 'http://localhost:3000/stories/1');
-  httpRequest.setRequestHeader('Content-Type', 'application/json');
-  httpRequest.setRequestHeader('Accept', 'application/json');
-  httpRequest.send(JSON.stringify(queryBox.value));
-}
-queryButton.addEventListener('click', addStory);
