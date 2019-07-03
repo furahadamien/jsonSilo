92ec25fa0f308b96f28e98d922380a3063519a48
Furaha <furahadamien30@gmail.com>
2019-07-03 13:56:05 -0400

    commit before modifying main.js

--- COMPLETE_SILO/web/main.js
+++ COMPLETE_SILO/web/main.js
@@ -17,7 +17,7 @@ let linkImage = document.querySelector('#hyperlinkImage');
 let textImage = document.querySelector('#textImage');
 let imageUrl = document.querySelector('#imageUrl');
 let error = document.querySelector('#error');
-let imageName = '';
+let imageName = ' ';
 const TEST_JSON =  {"name": {
   "type": "file",
   "value": "File",
@@ -25,6 +25,9 @@ const TEST_JSON =  {"name": {
     "menuitem": "NONE"
   }
 }};
+let dictstring = '';
+let dict = {"FullName" : '',
+    "image" : ""};
 
 function addImage(ev){
   let myFile = document.getElementById('myFile').files[0];
@@ -32,7 +35,10 @@ function addImage(ev){
   let Req = new XMLHttpRequest();
 
   Req.open("POST","/images", true);
+  imageName = Req.responseText;
+  console.log('first');
   Req.onload = function(oevent){
+    console.log(Req.status);
     if(Req.status == 200){
       let response = (Req.responseText);
       picture.src = `/photos/${response}`
@@ -40,7 +46,11 @@ function addImage(ev){
       imageUrl.textContent = window.location.href + `images/${response}`;
       linkImage.href = window.location.href + `images/${response}`;
       imageName = response;
-      console.log(imageName);
+      dict.image = response;
+      //dict = JSON.stringify(dict);
+      dictstring = JSON.parse(dict);
+      
+      
     }else if(Req.status == 204){
       error.textContent = 'wrong file format';
     }
@@ -48,16 +58,17 @@ function addImage(ev){
       textImage.textContent = 'something went wrong while uploading image';
     }
   };
-  console.log(imageName);
   Req.send(Data);
   ev.preventDefault()
 }
 
 function addStory(){ 
-  let dictstring = '';
+  
   let httpRequest = new XMLHttpRequest();
   httpRequest.onreadystatechange = function(){
-    let dict = {"FullName" : queryBox.value};
+    dict.FullName = queryBox.value;
+    //console.log(imageName);
+    console.log(dict.FullName);
     dict = JSON.stringify(dict);
     dictstring = JSON.parse(dict);
     if(httpRequest.readyState === XMLHttpRequest.DONE) {
@@ -82,5 +93,7 @@ function log(event){
   name.textContent = queryBox.value;
 }
 window.addEventListener("keyup", log);
+
 queryButton.addEventListener('click', addImage);
 queryButton.addEventListener('click', addStory);
+
